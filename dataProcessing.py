341ef5c312a07abb8588fc01d39cfdd0452ce3a5
thomasfoltz <thomasfoltz42@gmail.com>
2022-07-16 23:32:11 -0400

    legitDate function

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,11 +2,13 @@ import pandas as pd
 import yfinance as yf
 from datetime import date
 import os
-today = date.today()
 
 def pullTickerData(ticker, start, end):
-    tickerData = yf.download(ticker, start, end)
-    tickerData.to_csv(ticker + '_data.csv')
+    if legitDate(start) and legitDate(end):
+        tickerData = yf.download(ticker, start, end)
+        tickerData.to_csv(os.path.join('./', r'tickerData/' + ticker + '.csv'))
+    else:
+        print("Invalid start or end date")
 
 def formatDate(year, month, date):
     if month<10:
@@ -15,5 +17,30 @@ def formatDate(year, month, date):
         date = "0"+str(date)
     return "-".join([str(year), str(month), str(date)])
 
+def legitDate(start):
+    if len(start)!=10 or start[4]!='-' or start[7]!='-':
+        return False
+    if not start[0:3].isdigit() or not start[5:6].isdigit() or not start[8:9].isdigit():
+        return False
 
-pullTickerData('MSFT', formatDate(2022,5,1), formatDate(today.year, today.month, today.day))
+    year, month, day = int(start[0:4]), int(start[6:7]), int(start[8:10])
+    current_year, current_month, current_day = today.year, today.month, today.day
+
+    if year<1900 or year>current_year or month<1 or month>12 or day<1 or day>31:
+        return False
+    if (year==current_year and month>current_month) or (day>current_day and month==current_month and day>current_day):
+        return False
+    return True
+
+def updateAllTickers(tickers, start):
+    if legitDate(start):
+        for ticker in tickers:
+            pullTickerData(ticker, start, formatDate(today.year, today.month, today.day))
+    else:
+        print("Invalid start date")
+
+today = date.today()
+tickers = ['MSFT', 'AAPL', 'GOOGL']
+start_date = formatDate(2022, 1, 1)
+updateAllTickers(tickers, start_date)
+
