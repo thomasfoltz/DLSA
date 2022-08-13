a3c34d763a557b1ad325ab08e8bed842b3e50861
thomasfoltz <thomasfoltz42@gmail.com>
2022-08-13 15:21:13 -0400

    integrating js and css with flask

--- static/app.js
+++ static/app.js
@@ -1,5 +1,7 @@
 'use strict';
+
 const switcher = document.querySelector('.btn');
+
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
