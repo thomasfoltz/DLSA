b4253539bbf047f9a661cf9e1d11ba84c0fa592b
thomasfoltz <thomasfoltz42@gmail.com>
2022-12-24 15:47:24 -0500

    updating app.py and image dir

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
 import os
-from os.path import join, dirname, realpath
-import mysql.connector
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 UPLOAD_FOLDER = 'static/tickerData'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="dlsa",
-  database="databaseName"
-)
 
 @app.route("/")
 def index():
@@ -24,8 +15,7 @@ def index():
 def uploadFiles():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        uploaded_file.save(file_path)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename))
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
